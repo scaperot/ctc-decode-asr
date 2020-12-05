c746b748a979b31121ae772eede0ea565a83a725
David <scaperot@vt.edu>
2020-12-05 10:51:30 -0500

    adding prune algorithm from Gupta.

--- dali-lyric-analysis.py
+++ dali-lyric-analysis.py
@@ -6,12 +6,28 @@ import pdb
 import DALI as dali_code
 from DALI import utilities
 
-def line_length_hist_secs(data):
-    counts,bins = np.histogram(data)
-    span = (np.min(data),np.max(data))
-    nbins = span[1] - span[0]
-    plt.hist(data,bins=int(nbins),range=(0,20))
-    plt.show(block=False)
+import re
+
+#using Gupta's lyric preprocessing function...
+#need to check licensing, etc.
+def CleanUpLyrics(lyrics_raw):
+    line = lyrics_raw.lower()
+    regex = re.compile('[$*=:;/_,\.!?"\n]')
+    stripped_line = regex.sub('', line)
+    if stripped_line == '': return
+    check_for_bracket_words = stripped_line.split(' ')
+    non_bracket_words = []
+
+    for elem in check_for_bracket_words:
+        if elem == "": continue #remove extra space
+        if '(' in elem or ')' in elem: continue
+        if elem[-1] == '\'': elem = elem.replace('\'','g') #Check if "'" is at the end of a word, then replace it with "g", eg. makin' => making
+        if elem=="'cause": elem="cause"
+        elem=elem.replace('-',' ')
+        elem=elem.replace('&','and')
+        non_bracket_words.append(elem)
+    stripped_line = ' '.join(non_bracket_words)
+    return stripped_line.upper()
 
 if __name__ == '__main__':
     sr = 22050
@@ -20,10 +36,8 @@ if __name__ == '__main__':
 
     # ###############################################
     #
-    print("Measure the length of lines for 10 songs...")
-    #  max line length for 5358 songs: 700.2 secs
-    #  number of lines above 10 seconds: 605
-    #  time to process 5358 songs: 215.2 secs
+    print("Measure the length of lines for all songs...")
+    #  time to process 5358 songs: 232.2 seconds
     #
     # ###############################################
     allsongfilenames = utilities.get_files_path(dali_path,'.gz')
@@ -32,39 +46,45 @@ if __name__ == '__main__':
     n = len(allsongfilenames)
     start = time.time()
     alphabet = []
-    for i in range(n):
+    songs_with_numbers = []
+    for j in range(1000):
         #import song metadata
-        song_id =  os.path.relpath(allsongfilenames[i],dali_path).split('.')[0]
+        song_id =  os.path.relpath(allsongfilenames[j],dali_path).split('.')[0]
         dali_data = dali_code.get_the_DALI_dataset(dali_path,keep=[song_id])
         entry = dali_data[song_id]
 
         #get all lines of song
         annot = entry.annotations['annot']['lines']
         text = []
-        text = [i['text'] for i in dali_code.annot2frames(annot,1/sr)]
-        #pdb.set_trace()
+        text = [j['text'] for j in dali_code.annot2frames(annot,1/sr)]
+
+        # go through each line and find unique characters and add them
+        # to the alphabet
         for i in range(len(text)):
+            tmp1 = CleanUpLyrics(text[i])
+            if tmp1 == None:
+                continue
+            
             #transform to list of unique characters from text
-            #pdb.set_trace()
-            tmp = list(set(text[i]))
+            #tmp2 = list(set(text[i]))
+            tmp2 = list(set(tmp1))
+
+            searchfor = ['[',']']
+            if any(substring in tmp2 for substring in searchfor):
+                print('song:',j,', line:',i,text[i])
+                songs_with_numbers.append(j)
+                
 
             #add characters to alphabet, then remove duplicates
-            alphabet.extend(tmp)
+            alphabet.extend(tmp2)
             alphabet = list(set(alphabet))
 
 
     terminate = time.time()
     alphabet.sort()
 
-    #delta = np.array(delta)/sr
-    #print('max line length for',n,'songs:',np.max(delta))
-    #print('number of lines above 10 seconds:',np.sum(delta > 10))
     print('alphabet for DALI is:',''.join(alphabet))
     print('time to process',n,'songs:',terminate-start)
 
-
-    # plot histogram of values.
-    #plt.figure(2)
-    #line_length_hist_secs(delta)
-
     np.save('dali_alphabet.txt',''.join(alphabet))
+    np.save('dali_numeric.npy',songs_with_numbers)
