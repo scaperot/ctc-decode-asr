0c47cbe5d13fdfa62ec25a970859ba27c6e4d937
David <scaperot@vt.edu>
2020-12-11 23:07:25 -0500

    added file get_preprocess_dali_dataset which like the line to chunks script attempts to take a song and make cropped wav files from it.  this is the initial check in of this code.  it only is able to crop the files at the timesteps that are expected.  need to do the transcript magic to label things appropriately.

--- get_preprocess_dali_dataset.py
+++ get_preprocess_dali_dataset.py
@@ -10,13 +10,13 @@ from DALI import utilities
 
 
 '''
-Take a single song and break up into ~15s chunks in accordances with Stoller's 
-'End to end lyrics alignment for polyphonic music using an audio-to-character 
- recogition model.'
+Take a single song and break up into ~10s chunks 
+NOTE 1: This is a modification to Stoller's 'End to end lyrics alignment for polyphonic music 
+  using an audio-to-character recogition model.', in which this code does not use context windows 
+  on each side of the 10s prediction window.
 
-Data Metadata Format
-- 225501 samples @22050Hz (10.23s)
-
+Data Metadata Formata
+- 225501 samples @22050Hz (10.2268s)
 
 For Training: 
 - shift 112750 samples @22050Hz (5.11s)
@@ -52,11 +52,26 @@ def CleanUpLyrics(lyrics_raw):
     return stripped_line
 
 
-def download_song(info, dest, song_id):
-    basename = dest + '/' + song_id
+def download_song(song_id, dali_info, audio_path, sample_rate):
+    '''
+    download_song: 
+        1. download <song_id> from youtube.com
+        2. save as audio/<song_id>.mp3
+        3. convert to audio/<song_id>.wav (resample at sample_rate too)
+
+    Input: 
+    song_id   - (string) DALI song ID 
+    dali_info - (DALI object) DALI song information needed to download song (URL, etc.)
+    path      - (string) absolute path of destination for audio files
+    sample_rate - (int) sampling rate used when converting to .wav
+
+    Return:
+    N/A
+    '''
+    basename = audio_path + '/' + song_id
 
     # download...
-    errors = dali_code.get_audio(info, dest, skip=[], keep=[song_id])
+    errors = dali_code.get_audio(dali_info, audio_path, skip=[], keep=[song_id])
     print(errors)
     i = 0
 
@@ -64,11 +79,11 @@ def download_song(info, dest, song_id):
     while utilities.check_file(basename + '.mp3') != True and i < 10:
         time.sleep(1)
         i += 1
-        print(i)
+        print('Waiting:',i,'seconds...')
 
-    # convert to .wav...
+    # convert to .wav and resample to sample_rate
     print('Creating', basename + '.wav')
-    subprocess.call(['ffmpeg', '-i', basename + '.mp3', basename + '.wav'])
+    subprocess.call(['ffmpeg', '-i', basename + '.mp3', '-ar', str(sample_rate), basename + '.wav'])
 
     # remove mp3...
     print('Removing', basename + '.mp3')
@@ -76,35 +91,87 @@ def download_song(info, dest, song_id):
     return
 
 
-def save_samples_wav(dest, song_id, line_num, x, line_samples, sr):
+def save_samples_wav(song_id, audio_path, id_num, x, window_samples, sr):
     '''
-    song_id - string of DALI song ID
-    line    - line number
-    x - song samples
-    line_samples  - tuple (start,end)
-    dest - folder to save to
+    Inputs:
+    song_id       (string) DALI song ID, used as basename
+    id_num        (int) used to append to the name of the file.  can be something specific like line number
+                         or just an arbitrary counter.
+    x             (numpy array) song samples of entire song
+    window_samples  (tuple) (start,end) window of samples to save to disk
+    dest          (string) folder to save to
+
+    Return:
+    filename with no path      (string)
+    absolute path and filename (string)
     '''
     n = x.shape[0]
-    if line_num < 10:
-        line_str = str(line_num).zfill(2)
-    elif line_num < 100:
-        line_str = str(line_num).zfill(1)
+    if id_num < 10:
+        id_str = str(id_num).zfill(2)
+    elif id_num < 100:
+        id_str = str(id_num).zfill(1)
     else:
-        print('This thing has more than 100 lines!!')
-        line_str = str(line_num)
+        id_str = str(line_num)
 
-    basename = dest + '/' + song_id + '_' + line_str
-    start = line_samples[0]
-    term = line_samples[1]
-
-    print('Writing:', basename + '.wav')
+    basename = audio_path + '/' + song_id + '_' + id_str
+    start = window_samples[0]
+    term = window_samples[1]
+    
+    print('Writing:', basename + '.wav,',window_samples)
     sf.write(basename + '.wav', x[start:term], sr)
-    return (basename + '.wav'), ('audio/' + song_id + '_' + line_str + '.wav')
+    return (basename + '.wav'), ('audio/' + song_id + '_' + id_str + '.wav')
 
     # n%window crops the file to a multiple of the window
     # reshape creates a 2-d matrix with rows being number of chunks and 
     # columns being the window (i.e. number of samples)
     # out = np.reshape(x[:-(n%samples)],(np.floor(n/samples).astype(int),samples))
+
+def append_transcript(full_filename,duration,transcript):
+    '''
+    append_transcript: save in nemo manifest format
+       full_filename: absolute path to audio file
+       duration:      length of song at full_filename
+       transcript:    lyrics corresponding to full_filename
+    '''
+    jsonfile = open(filename, 'a')
+    line_format = "{}\"audio_filepath\": \"{}\", \"duration\": {}, \"text\": \"{}\"{}"
+    jsonfile.write(line_format.format(
+            "{", full_filename, duration, transcript, "}\n"))
+    jsonfile.close()
+    return
+
+def crop_song(song_id, audio_path, dali_entry, win_samples):
+    '''
+    crop_song - takes a DALI song and crops it m times into win_length samples.
+
+    Inputs: 
+       song_id    - DALI song id
+       entry      - DALI data entry
+       audio_path - absolute path where audio files are stored (read/write)
+       win_samples - number of samples for each crop
+    Return:
+       song_ndx   - (m,start_sample,stop_sample) indices for the m crops
+
+    1. load song with librosa
+    2. calculate indices (i.e. sample index starting at 0) for windows of chunks. 
+       a. 'win_rate' is win_length/2
+       b. do not keep parts of the song less than win_length
+    3. crop according to indices and save to audio_path in the form
+        audio/<song_id>_##.wav 
+        where ## is the number of chunks in the song.
+    '''
+    x, sr = librosa.load(audio_path + '/' + song_id + '.wav', sr=None)
+    n   = np.arange(x.shape[0])  # counter from 0 to max samples of x
+    ndx = n[::win_samples][:-1] # takes every win_samples of n, then remove the last sample
+    start_ndx = np.reshape(ndx,(ndx.shape[0],1))
+    end_ndx   = start_ndx+win_samples
+    l = start_ndx.shape[0]
+
+    for i in range(l):
+        save_samples_wav(song_id, audio_path, i, x, (start_ndx[i][0],end_ndx[i][0]), sr)
+
+
+
 
 
 def download_convert_song(song_id, dali_data, dali_info, audio_path):
@@ -167,12 +234,30 @@ def is_song_trainable(dali_data):
     '''
     filter out songs by metadata
     - only do english lyrics for now. (~75% of DALI dataset)
-    - TODO: blacklist for poorly transcribed songs (manual analysis)
+    - TODO: blacklist (i.e. load a list of songs to ignore) poorly transcribed songs
+            (manual analysis)
 
     '''
     language = dali_data.info['metadata']['language']
     if language != 'english':
+        print('is_song_trainable: NOT ENGLISH')
         return False
+    
+    #annotation type is generic to all labels (AFAIK)
+    #
+    #annot_type = dali_data.annotations['type']
+    #if annot_type != 'horizontal':
+    #    return False
+    
+    #number of lines should be ok as long as the songs are 
+    # shuffled when performing training.  its possible 
+    # they are signifcantly longer than other songs, there
+    # would be some overfit to the distribution, but that
+    # would have to be pretty large.
+    #annot = dali_data.annotations['annot']['lines']
+    #if len(annot) > 100:
+    #    return False
+    #
     # TODO: Possibly filter out more songs, like those that have long instrumental sections
     # print(dali_data.annotations.keys())
     # print(dali_data.annotations['annot_param'])
@@ -182,8 +267,8 @@ def is_song_trainable(dali_data):
     # print([(i['time'], i['text']) for i in dali_code.annot2frames(annot, 1/22050)])
     return True
 
+def dali_setup():
 
-if __name__ == '__main__':
     ################################
     # CHECK FOR DALI dataset folder
     ################################
@@ -203,13 +288,36 @@ if __name__ == '__main__':
         os.makedirs(os.path.abspath('.') + '/audio/')
 
     dali_info = dali_code.get_info(dali_path + '/info/DALI_DATA_INFO.gz')
+    return dali_path, audio_path, dali_info
 
-    # ###############################################################
-    #
+def preprocess_song(song_id, dail_path, audio_path, dali_info, sample_rate):
+    '''
+    
+    '''
+    win_size = 10.2268
+    win_samples = np.floor(win_size * sample_rate).astype(int)
+    print('window samples: ',win_samples)
+
+    dali_entry = dali_code.get_the_DALI_dataset(dali_path, keep=[song_id])[song_id]
+    if is_song_trainable(dali_entry):
+
+        # get song and save to audio_path
+        download_song(song_id, dali_info, audio_path, sample_rate)
+
+        # slice up song and save to audio_path, return indices of samples
+        indices = crop_song(song_id, audio_path, dali_entry, win_samples)
+
+        # slice up the transcript for each cropped version of the song
+        # calculate the duration
+        # save to 
+        return True
+
+    return False
+
+def main():
+    '''
     print("Import a single song and chop into chunks at the line level...")
-    #
-    # ###############################################################
-
+    '''
     # 1. select a random song from the dali dataset, 
     # 2. create a set overlapping .wav files  from the song
     # 3. create a set of transcripts for each .wav file.
@@ -220,13 +328,35 @@ if __name__ == '__main__':
 
     i = np.random.randint(n)
     song_id = os.path.relpath(allsongfilenames[i], dali_path).split('.')[0]
-    dali_data = dali_code.get_the_DALI_dataset(dali_path, keep=[song_id])[song_id]
-    if is_song_trainable(dali_data):
-        converted = download_convert_song(song_id, dali_data, dali_info, audio_path)
-        if converted:
-            break
-        num_skipped_after_download += 1
-        if num_skipped_after_download >= 10:
-            print("Too many songs downloaded but not converted; aborting ... ")
-            break
+    entry = dali_code.get_the_DALI_dataset(dali_path, keep=[song_id])[song_id]
+    while True:
+        if is_song_trainable(dali_data):
+            converted = download_convert_song(song_id, entry, dali_info, audio_path)
+            if converted:
+                break
+            num_skipped_after_download += 1
+            if num_skipped_after_download >= 10:
+                print("Too many songs downloaded but not converted; aborting ... ")
+                break
+
+
+if __name__ == '__main__':
+    '''
+    choose a random song, crop audio files, and massage transcripts
+    '''
+    dali_path, audio_path, dali_info = dali_setup()
+    allsongfilenames = utilities.get_files_path(dali_path, '.gz')
+    n = len(allsongfilenames)
+
+    #number of samples assumes 22kHz (Stoller) - 10.23s
+    sample_rate = 22050 
+
+    #choose a random song...
+    i = np.random.randint(n)
+    song_id = os.path.relpath(allsongfilenames[i], dali_path).split('.')[0]
+    
+    #preprocess a song
+    if not preprocess_song(song_id, dali_path, audio_path, dali_info, sample_rate):
+        print('ERROR PREPROCESSING.')
+
 
